b67945ff04229b60e10b0695cca8a11b06050164
yagocrispim0 <yagocrispimgomes@gmail.com>
2022-01-22 14:16:07 -0300

    Created POSITION class

---
+++ Chess-Console/Board/Position.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Board
+{
+    internal class Position
+    {
+        public int Line { get; set; }
+        public int Column { get; set; }
+
+        public Position()
+        {
+        }
+        public Position(int line, int column)
+        {
+            Line = line;
+            Column = column;
+        }
+
+        public override string ToString()
+        {
+            return Line
+                + ", "
+                + Column;
+        }
+    }
+}
