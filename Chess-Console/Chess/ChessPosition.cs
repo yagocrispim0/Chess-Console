6683ce60f4a0313e7ef0dc667275aab71e5fb537
yagocrispim0 <yagocrispimgomes@gmail.com>
2022-01-23 09:13:02 -0300

    Creating ChessPosition class

---
+++ Chess-Console/Chess/ChessPosition.cs
@@ -0,0 +1,28 @@
+﻿
+// Class designed to handle the conversion between a a matrix index and the real-life chess positions. 
+using Chessboard;
+
+namespace Chess
+{
+    internal class ChessPosition
+    {
+        public char Column { get; set; }
+        public int Line { get; set; }
+
+        public ChessPosition(char column, int line)
+        {
+            Column = column;
+            Line = line;
+        }
+
+        public Position toPosition()
+        {
+            return new Position(8 - Line, Column - 'a');
+        }
+
+        public override string ToString()
+        {
+            return "" + Line + Column;
+        }
+    }
+}
